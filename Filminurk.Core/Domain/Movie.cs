51ec22d444d839afe3f3fb5923ca06b3e0df0c7f
Opilane TTHK <opilane@tthk.ee>
2025-10-09 11:05:35 +0300

    Movies modeldata for both domain and  viewmodels

---
+++ Filminurk.Core/Domain/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Domain
+{
+   
+    public class Movie
+    {
+       public Guid ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateOnly FirstPublished { get; set; }
+        public string Director { get; set; }
+        public List<string>? Actors { get; set; }
+        public decimal? CurrentRating { get; set; }
+        //public List<UserComment>? Reviews { get; set; }
+        public int? OscarsWon { get; set; }
+        public decimal? RottenTomatoes { get; set; }
+        public decimal? IMDbRating { get; set; }
+       
+
+    }   
+
+
+}
---
+++ Filminurk/Models/Movies/MoviesIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Filminurk.Models.Movies
+{
+    public class MoviesIndexViewModel
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        
+        public DateOnly FirstPublished { get; set; }
+        public decimal? CurrentRating { get; set; }
+        //public List<UserComment>? Reviews { get; set; }
+        public int? OscarsWon { get; set; }
+        public decimal? IMDbRating { get; set; }
+    }
+}
