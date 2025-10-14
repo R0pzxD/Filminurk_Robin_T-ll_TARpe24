8591b5bbbf78894c7a91ace63c01781055ed6b99
R0pzxD <138902463+R0pzxD@users.noreply.github.com>
2025-10-14 13:54:14 +0300

    brokemyproject

---
+++ Filminurk.Core/ServiceInterface/IMovieServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Filminurk.Core.Domain;
+using Filminurk.Core.Dto;
+
+namespace Filminurk.Core.ServiceInterface
+{
+    public interface IMovieServices
+    {
+        Task<Movie> Create(MoviesDTO dTO);
+        Task<Movie> DetailsAsync(Guid id);
+        
+        
+    }
+}
